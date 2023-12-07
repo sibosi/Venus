23048193324a1073c0479fd23f47ba9296957ea7
Sibosi <siminimi2008@gmail.com>
2023-12-07 19:27:01 +0100

    Simple Chat

---
+++ client.py
@@ -0,0 +1,21 @@
+import socket
+
+def send_message(message):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        # Connect to the server
+        client_socket.connect(("localhost", 8080))
+
+        # Send the message
+        client_socket.sendall(message.encode())
+
+        # Receive the response
+        data = client_socket.recv(1024).decode()
+        print(f"Received message from server: {data}")
+
+
+if __name__ == "__main__":
+    # Get the message from the user
+    message = input("Enter the message: ")
+
+    # Send the message to the server
+    send_message(message)
---
+++ server.py
@@ -0,0 +1,21 @@
+import socket
+import threading
+
+HOST = "0.0.0.0"
+PORT = 8080
+
+def handle_client(client_socket):
+
+    data = client_socket.recv(1024)
+    print("Received message from client:", data)
+    client_socket.sendall(data)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+    server_socket.bind((HOST, PORT))
+    server_socket.listen()
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        thread = threading.Thread(target=handle_client, args=(client_socket,))
+        thread.start()
+        server_socket.listen()
