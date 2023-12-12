ad3eb4489b8cfb7ba96eed938a5110f00bed2e37
Sibosi <siminimi2008@gmail.com>
2023-12-12 21:37:02 +0100

    More configuration option
    New thigs:
    - README.md (in progress)
    - more configuration option for the update.py
    - log.txt

---
+++ README.md
@@ -0,0 +1,13 @@
+# Venus
+Szerver - kliens projekt egy Raspberry Pi-n.
+
+A szerver üzenet és file fogadásra alkalmas.
+
+## Használat
+- `settings.json` - konfigurálja a hálózatok beállításait
+
+- `main.py` - a konfigurált hálózatokat betölti, futtatja
+
+- `update.py` - frissíti a file-okat a GitHub-ról
+
+- `check-in.py` - frissítés 
--- update.py
+++ update.py
@@ -1,16 +1,35 @@
 from datetime import datetime
-print(f'{datetime.now()} | Venus update started')
-
-
+import json
 import os
 import subprocess
+import sys
 
+
+with open('settings.json', 'r') as file:
+    settings = json.load(file)
+
+def print_update(*a):
+    if settings["update printing"]:
+        print(*a)
+    if settings["update logging"]:
+        with open('storage/log.txt', 'a') as file:
+            file.write(*a)
+            file.write('\n')
+
+def subprocess_update(command):
+        process = subprocess.Popen(command, stdout=subprocess.PIPE)
+        output, err = process.communicate()
+
+        print_update(str(output))    
+
+
+print_update(f'{datetime.now()} | Venus update started')
 
 repo_path = os.path.dirname(__file__)
 os.chdir(repo_path)
-subprocess.run(['git', 'pull'])
+subprocess_update(['git', 'pull'])
 
+print_update(f'{datetime.now()} | Venus update ended\n')
 
-print(f'{datetime.now()} | Venus update ended\n')
-
-#subprocess.run(['python', 'check-in.py', '>>',  '~/log.txt',  '&'])
+if settings["check-in after update"]:
+    subprocess.run(['python', 'check-in.py', '>>',  'storage/log.txt',  '&'])
